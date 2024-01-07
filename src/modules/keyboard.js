720a419ea9b9edaee43ad2ae3f8e39682abea6cc
aoneban <torgregion1@gmail.com>
2024-01-08 00:14:36 +0100

    feat: add search target element and change color buttons

--- src/modules/keyboard.js
+++ src/modules/keyboard.js
@@ -1,5 +1,25 @@
 import data from './data';
 
+const changeColorToButton = (elem1, elem2) => {
+  const itemOne = elem1;
+  itemOne.style.opacity = 1;
+  itemOne.parentNode.style.border = 'none';
+  const itemTwo = elem2;
+  itemTwo.style.backgroundColor = '#9316E1';
+};
+
+const searchForRequiredLetter = (e) => {
+  const currentClick = e.currentTarget.innerText;
+  const currentElem = e.currentTarget;
+  currentElem.style.backgroundColor = '#ea4f4f';
+  const nodeList = document.querySelectorAll('.letter-class');
+  for (let i = 0; i < nodeList.length; i += 1) {
+    if (nodeList[i].innerText === currentClick) {
+      changeColorToButton(nodeList[i], currentElem);
+    } 
+  }
+};
+
 export const generateKeyboard = (n) => {
   const keyboardWrapper = document.querySelector('.block-right__keyboard');
   if (n > data.length - 1) {
@@ -12,13 +32,7 @@ export const generateKeyboard = (n) => {
     key.classList.add('key', `${el.keyCode}`);
     key.textContent = el.caseUpEn;
     key.addEventListener('click', (event) => {
-      const currentClick = event.currentTarget.innerText;
-      const nodeList = document.querySelectorAll('.letter-class');
-      for (let i = 0; i < nodeList.length; i += 1) {
-        if (nodeList[i].innerText === currentClick) {
-          nodeList[i].style.opacity = 1;
-        }
-      }
+      searchForRequiredLetter(event);
     });
     keyRows.append(key);
   });
