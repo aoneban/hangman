bda14d2dd83db0656f26692980c083c9eecdc6b2
aoneban <torgregion1@gmail.com>
2024-01-15 22:11:52 +0100

    feat: add adaptive layouts to the project

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,15 +2,15 @@ import '../styles/modal.scss';
 // eslint-disable-next-line import/no-cycle
 import { launchNewGame } from './keyboard';
 
-
-
 export const modalWindow = (bool, word) => {
   const myModal = document.createElement('div');
   myModal.setAttribute('id', 'myModal');
   myModal.classList.add('modal');
+
   const content = document.createElement('div');
   content.classList.add('modal-content');
   content.setAttribute('id', 'mod01');
+
   const caption = document.createElement('div');
   caption.classList.add('caption');
 
@@ -19,12 +19,16 @@ export const modalWindow = (bool, word) => {
   const textToModal = document.createElement('h2');
   // eslint-disable-next-line no-unused-expressions
   bool === true ? (textToModal.textContent = 'You won!') : (textToModal.textContent = 'You lose!');
+
   const hiddenWord = document.createElement('p');
   hiddenWord.textContent = `The hidden word was: ${word}`;
+
   const contain = document.createElement('p');
   contain.textContent = 'Want to play again?';
+
   const wrapperButtons = document.createElement('div');
   wrapperButtons.classList.add('wrapper-buttons');
+  
   const buttonOne = document.createElement('button');
   buttonOne.addEventListener('click', launchNewGame);
   buttonOne.classList.add('btn');
